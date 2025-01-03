8a741d93c32d9c1a77150ad9f5cf923c7cf90ff7
Ezzz-Lui <ogn.lui@gmail.com>
2025-01-02 22:54:19 -0600

    Add development stack data with associated icons

---
+++ src/data/stack.ts
@@ -0,0 +1,51 @@
+import Astro from "@/icons/astro"
+import MicrosoftAzure from "@/icons/azure"
+import Django from "@/icons/django"
+import Docker from "@/icons/docker"
+import JavaScript from "@/icons/javascript"
+import Nextjs from "@/icons/nextjs"
+import PostgreSQL from "@/icons/postgresql"
+import Python from "@/icons/python"
+import Supabase from "@/icons/supabase"
+import TailwindCSS from "@/icons/tailwind"
+import TypeScript from "@/icons/typescript"
+import Vercel from "@/icons/vercel"
+import Github from "@/icons/github"
+import NestJS from "@/icons/nestjs"
+import ReactIcon from "@/icons/react"
+import shadcnui from "@/icons/shadcn"
+import MicrosoftSQLServer from "@/icons/sqlserver"
+import Slack from "@/icons/slack"
+import JetBrainsPyCharm from "@/icons/pycharm"
+import Microsoft from "@/icons/microsoft"
+import Zod from "@/icons/zod"
+import Vite from "@/icons/vite"
+import MongoDB from "@/icons/mongo"
+
+export const projects = [
+  { 
+    title: 'My development stack', 
+    skills: [
+      { icon: Django },
+      { icon: Python },
+      { icon: TailwindCSS },
+      { icon: TypeScript },
+      { icon: Nextjs },
+      { icon: PostgreSQL },
+      { icon: JavaScript },
+      { icon: Docker },
+      { icon: Supabase },
+      { icon: Vercel },
+      { icon: Astro },
+      { icon: Github },
+      { icon: NestJS },
+      { icon: ReactIcon },
+      { icon: shadcnui },
+      { icon: Slack },
+      { icon: JetBrainsPyCharm },
+      { icon: Microsoft },
+      { icon: MongoDB }
+    ]
+  }
+]
+
