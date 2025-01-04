3486696c3e81e8d76e0ca9a74c9d13c4b0b1c579
Ezzz-Lui <ogn.lui@gmail.com>
2025-01-04 00:45:21 -0600

    Refactor Vercel icon component to support dynamic theming based on current theme state

--- src/icons/vercel.tsx
+++ src/icons/vercel.tsx
@@ -1,4 +1,33 @@
+"use client";
+
 import * as React from "react";
 import type { SVGProps } from "react";
-const Vercel = (props: SVGProps<SVGSVGElement>) => <svg viewBox="0 0 256 222" width="1em" height="1em" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" {...props}><path fill="#fff" d="m128 0 128 221.705H0z" /></svg>;
+import { useTheme } from "next-themes";
+
+const Vercel = (props: SVGProps<SVGSVGElement>) => {
+  const { theme } = useTheme();
+  const [currentTheme, setCurrentTheme] = React.useState<string | undefined>(undefined);
+
+  React.useEffect(() => {
+    setCurrentTheme(theme);
+  }, [theme]);
+
+  return (
+    <svg
+      viewBox="0 0 256 222"
+      width="1em"
+      height="1em"
+      xmlns="http://www.w3.org/2000/svg"
+      preserveAspectRatio="xMidYMid"
+      className={currentTheme === "dark" ? "svg-dark" : "svg-light"}
+      {...props}
+    >
+      <path
+        fill={currentTheme === "dark" ? "#fff" : "#000"} // Cambia el color según el tema
+        d="m128 0 128 221.705H0z"
+      />
+    </svg>
+  );
+};
+
 export default Vercel;
