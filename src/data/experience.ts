2097a7641f1770350bf458e67aa0aa9a37d60ce4
Ezzz-Lui <ogn.lui@gmail.com>
2025-03-31 21:13:20 -0600

    Add new icons and update experience descriptions for enhanced UI integration

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,39 +1,100 @@
+import Astro from "@/icons/astro"
+import MicrosoftAzure from "@/icons/azure"
+import Django from "@/icons/django"
+import Docker from "@/icons/docker"
+import JavaScript from "@/icons/javascript"
+import Nextjs from "@/icons/nextjs"
+import PostgreSQL from "@/icons/postgresql"
+import Python from "@/icons/python"
+import Supabase from "@/icons/supabase"
+import TailwindCSS from "@/icons/tailwind"
+import TypeScript from "@/icons/typescript"
+import Vercel from "@/icons/vercel"
+import Vue from "@/icons/vue"
+import Vuetify from "@/icons/vuetify"
+import MySQL from "@/icons/mysql"
+import Swagger from '@/icons/swagger';
+import React from "@/icons/react"
+import NestJS from "@/icons/nestjs"
+import Nodejs from "@/icons/node"
+import MicrosoftSQLServer from "@/icons/sqlserver"
+import Microsoft from "@/icons/microsoft"
+import AWS from "@/icons/aws"
+
 export const experiences = [
     {
       title: 'Backend Python Developer',
       company: 'Tech Americas USA INC.',
       period: 'January 2025 - Present',
-      description: 'Backend and API development of scalable applications using Python, Django, Django Rest Framework and Docker. Data processing and analysis with Pandas and Numpy.'
+      description: 'Backend and API development of scalable applications using Python, Django, Django Rest Framework, Swagger and Docker. Frontend development with Vue.js and Vuetify components, and documentation with Astro and Sarlight for projects.',
+      skills: [
+        { name: 'Django', icon: Django },
+        { name: 'Python', icon: Python },
+        { name: 'Vue', icon: Vue },
+        { name: 'Vuetify', icon: Vuetify },
+        { name: 'MySQL', icon: MySQL },
+        { name: 'Docker', icon: Docker },
+        { name: 'Astro', icon: Astro },
+      ]
     },
     {
       title: 'Intern - Lead Full Stack Developer',
       company: 'ITCA - FEPADE',
-      period: '2024 - Present',
-      description: 'Development of fullstack applications for national institutions from databases, cloud computing, DevSecOps, DevOps and frontend-backend development.'
+      period: '2024 - 2025',
+      description: 'Development of fullstack applications for national institutions from databases, cloud computing, DevSecOps, DevOps and frontend-backend development.',
+      skills: [
+        { name: 'Typescript', icon: TypeScript },
+        { name: 'Javascript', icon: JavaScript },
+        { name: 'Next', icon: Nextjs },
+        { name: 'React', icon: React },
+        { name: 'Nest', icon: NestJS },
+      ]
     },
     {
       title: 'Intern - Web Developer',
       company: 'Mabrick',
-      period: 'March 2024',
-      description: 'Development of scalable new official web applications using React and Node.js. Implemented CI/CD pipelines and deployed applications on Vercel'
+      period: 'March 2024 - July 2024',
+      description: 'Development of scalable new official web applications using React and Node.js. Implemented CI/CD pipelines and deployed applications on Vercel',
+      skills: [
+        { name: 'Astro', icon: Astro },
+        { name: 'TailwindCSS', icon: TailwindCSS },
+        { name: 'Vercel', icon: Vercel },
+        { name: 'Javascript', icon: JavaScript },
+        { name: 'NodeJs', icon: Nodejs },
+      ]
     },
     {
       title: 'Developer Junior Trainee',
       company: 'Kata Software ES',
       period: '2023 - 2024',
-      description: 'SQL Development, Reporting with Report Builder and SQL Server, SQL Databases and Servers for dynamic reports, and data extraction.'
+      description: 'SQL Development, Reporting with Report Builder and SQL Server, SQL Databases and Servers for dynamic reports, and data extraction.',
+      skills: [
+        { name: 'SQL Server', icon: MicrosoftSQLServer },
+        { name: 'Report builder', icon: Microsoft },
+        { name: 'Microsoft Azure', icon: MicrosoftAzure },
+        { name: 'Amazon Web Services', icon: AWS },
+      ]
     },
     {
       title: 'Developer Trainee',
       company: 'CityLab',
       period: '2023 - 2024',
-      description: 'SQL querys and DB relational servers, datasets with SQL Server and query optimization for data sources.'
+      description: 'SQL querys and DB relational servers, datasets with SQL Server and query optimization for data sources.',
+      skills: [
+        { name: 'SQL Server', icon: MicrosoftSQLServer },
+        { name: 'Report builder', icon: Microsoft },
+        { name: 'Microsoft Azure', icon: MicrosoftAzure },
+        { name: 'Amazon Web Services', icon: AWS },
+      ]
     },
     {
       title: 'Intern - SQL Developer Jr',
       company: 'CEL - Comision Ejecutiva Hidroelectrica del Rio Lempa',
       period: '2023',
-      description: 'ORACLE database fundamentals, Data modeling in PL/SQL, Reporting and documentation of systems and PLSQL.'
+      description: 'ORACLE database fundamentals, Data modeling in PL/SQL, Reporting and documentation of systems and PLSQL, first steps with Oracle SQL and PL/SQL for use data modeler and querys for extract data. Review documentation from existing systems and refactor code for json data.',
+      skills: [
+        
+      ]
     }
   ]
   
