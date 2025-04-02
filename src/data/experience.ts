0745742ab850c9872c8e977c2db6b32b1ec5b84f
Ezzz-Lui <ogn.lui@gmail.com>
2025-04-02 02:56:11 -0600

    Update experience descriptions to enhance clarity and detail for improved skill representation

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -22,80 +22,81 @@ import Microsoft from "@/icons/microsoft"
 import AWS from "@/icons/aws"
 
 export const experiences = [
-    {
-      title: 'Backend Python Developer',
-      company: 'Tech Americas USA INC.',
-      period: 'January 2025 - Present',
-      description: 'Backend and API development of scalable applications using Python, Django, Django Rest Framework, Swagger and Docker. Frontend development with Vue.js and Vuetify components, and documentation with Astro and Sarlight for projects.',
-      skills: [
-        { name: 'Django', icon: Django },
-        { name: 'Python', icon: Python },
-        { name: 'Vue', icon: Vue },
-        { name: 'Vuetify', icon: Vuetify },
-        { name: 'MySQL', icon: MySQL },
-        { name: 'Docker', icon: Docker },
-        { name: 'Astro', icon: Astro },
-      ]
-    },
-    {
-      title: 'Intern - Lead Full Stack Developer',
-      company: 'ITCA - FEPADE',
-      period: '2024 - 2025',
-      description: 'Development of fullstack applications for national institutions from databases, cloud computing, DevSecOps, DevOps and frontend-backend development.',
-      skills: [
-        { name: 'Typescript', icon: TypeScript },
-        { name: 'Javascript', icon: JavaScript },
-        { name: 'Next', icon: Nextjs },
-        { name: 'React', icon: React },
-        { name: 'Nest', icon: NestJS },
-      ]
-    },
-    {
-      title: 'Intern - Web Developer',
-      company: 'Mabrick',
-      period: 'March 2024 - July 2024',
-      description: 'Development of scalable new official web applications using React and Node.js. Implemented CI/CD pipelines and deployed applications on Vercel',
-      skills: [
-        { name: 'Astro', icon: Astro },
-        { name: 'TailwindCSS', icon: TailwindCSS },
-        { name: 'Vercel', icon: Vercel },
-        { name: 'Javascript', icon: JavaScript },
-        { name: 'NodeJs', icon: Nodejs },
-      ]
-    },
-    {
-      title: 'Developer Junior Trainee',
-      company: 'Kata Software ES',
-      period: '2023 - 2024',
-      description: 'SQL Development, Reporting with Report Builder and SQL Server, SQL Databases and Servers for dynamic reports, and data extraction.',
-      skills: [
-        { name: 'SQL Server', icon: MicrosoftSQLServer },
-        { name: 'Report builder', icon: Microsoft },
-        { name: 'Microsoft Azure', icon: MicrosoftAzure },
-        { name: 'Amazon Web Services', icon: AWS },
-      ]
-    },
-    {
-      title: 'Developer Trainee',
-      company: 'CityLab',
-      period: '2023 - 2024',
-      description: 'SQL querys and DB relational servers, datasets with SQL Server and query optimization for data sources.',
-      skills: [
-        { name: 'SQL Server', icon: MicrosoftSQLServer },
-        { name: 'Report builder', icon: Microsoft },
-        { name: 'Microsoft Azure', icon: MicrosoftAzure },
-        { name: 'Amazon Web Services', icon: AWS },
-      ]
-    },
-    {
-      title: 'Intern - SQL Developer Jr',
-      company: 'CEL - Comision Ejecutiva Hidroelectrica del Rio Lempa',
-      period: '2023',
-      description: 'ORACLE database fundamentals, Data modeling in PL/SQL, Reporting and documentation of systems and PLSQL, first steps with Oracle SQL and PL/SQL for use data modeler and querys for extract data. Review documentation from existing systems and refactor code for json data.',
-      skills: [
-        
-      ]
-    }
+  {
+    title: 'Backend Python Developer',
+    company: 'Tech Americas USA INC.',
+    period: 'January 2025 - Present',
+    description: 'Backend and API development of scalable applications using Python, Django, Django Rest Framework, Swagger and Docker. Frontend development with Vue.js and Vuetify components, and documentation with Astro and Sarlight for projects. Collaborated with cross-functional teams to design and implement efficient solutions, ensuring high performance and scalability.',
+    skills: [
+    { name: 'Django', icon: Django },
+    { name: 'Python', icon: Python },
+    { name: 'Vue', icon: Vue },
+    { name: 'Vuetify', icon: Vuetify },
+    { name: 'MySQL', icon: MySQL },
+    { name: 'Docker', icon: Docker },
+    { name: 'Astro', icon: Astro },
+    ]
+  },
+  {
+    title: 'Intern - Lead Full Stack Developer',
+    company: 'ITCA - FEPADE',
+    period: '2024 - 2025',
+    description: 'Development of fullstack applications for national institutions from databases, cloud computing, DevSecOps, DevOps and frontend-backend development. Led a team of developers to deliver high-quality software solutions with NextJs and Vercel, develop relational database with SQL Server and systems deploy on Azure, ensuring adherence to best practices and project timelines.',
+    skills: [
+    { name: 'Typescript', icon: TypeScript },
+    { name: 'Javascript', icon: JavaScript },
+    { name: 'Next', icon: Nextjs },
+    { name: 'Vercel', icon: Vercel },
+    { name: 'React', icon: React },
+    { name: 'Nest', icon: NestJS },
+    ]
+  },
+  {
+    title: 'Intern - Web Developer',
+    company: 'Mabrick',
+    period: 'March 2024 - July 2024',
+    description: 'Development of scalable new official web applications using React and Node.js. Implemented CI/CD pipelines and deployed applications on Vercel. Develop optimized application performance using Microsoft Clarity for insights.',
+    skills: [
+    { name: 'Astro', icon: Astro },
+    { name: 'TailwindCSS', icon: TailwindCSS },
+    { name: 'Vercel', icon: Vercel },
+    { name: 'Javascript', icon: JavaScript },
+    { name: 'NodeJs', icon: Nodejs },
+    ]
+  },
+  {
+    title: 'Developer Junior Trainee',
+    company: 'Kata Software ES',
+    period: '2023 - 2024',
+    description: 'SQL Development, Reporting with Report Builder and SQL Server, SQL Databases and Servers for dynamic reports, and data extraction. Assisted in the optimization of database queries and the creation of detailed technical documentation for internal use.',
+    skills: [
+    { name: 'SQL Server', icon: MicrosoftSQLServer },
+    { name: 'Report builder', icon: Microsoft },
+    { name: 'Microsoft Azure', icon: MicrosoftAzure },
+    { name: 'Amazon Web Services', icon: AWS },
+    ]
+  },
+  {
+    title: 'Intern Developer Trainee ',
+    company: 'CityLab',
+    period: '2023 - 2024',
+    description: 'SQL querys and DB relational servers, datasets with SQL Server and query optimization for data sources. Supported the development of data-driven applications and provided insights for improving database performance.',
+    skills: [
+    { name: 'SQL Server', icon: MicrosoftSQLServer },
+    { name: 'Report builder', icon: Microsoft },
+    { name: 'Microsoft Azure', icon: MicrosoftAzure },
+    { name: 'Amazon Web Services', icon: AWS },
+    ]
+  },
+  {
+    title: 'Intern - SQL Developer Jr',
+    company: 'CEL - Comision Ejecutiva Hidroelectrica del Rio Lempa',
+    period: '2023',
+    description: 'ORACLE database fundamentals, Data modeling in PL/SQL, Reporting and documentation of systems and PLSQL, first steps with Oracle SQL and PL/SQL for use data modeler and querys for extract data. Review documentation from existing systems and refactor code for json data. Gained hands-on experience in database management and contributed to the enhancement of existing systems.',
+    skills: [
+    
+    ]
+  }
   ]
   
   
