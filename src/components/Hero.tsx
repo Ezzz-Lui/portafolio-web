8c89449976b83e691327781f6ab94cf6b21c31b8
Luis Ramos <ogn.lui@gmail.com>
2025-05-21 15:09:00 -0600

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,7 +22,7 @@ export function Hero() {
         <p className="text-xl md:text-3xl font-bold mb-6 dark:text-gray-300">
           <span className="opacity-70 dark:opacity-90">Software Developer</span> | 
           <span className="opacity-70 dark:opacity-80"> Azure AI Engineer Associate</span> | 
-          <span className="opacity-70 dark:opacity-70"> Data Analyst Jr</span>
+          <span className="opacity-70 dark:opacity-70"> Data Analyst</span>
         </p>
         <p className="mb-8 max-w-3xl text-base md:text-xl leading-relaxed text-gray-600 dark:text-gray-300">
           Technician with a background in Intelligent Computer Engineering, specialized in 
